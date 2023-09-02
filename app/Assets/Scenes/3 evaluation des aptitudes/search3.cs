1927b770854947d73a8af95356f492f7aed97db3
youki012 <141281362+youki012@users.noreply.github.com>
2023-09-02 13:34:55 +0100

    searcher

---
+++ app/Assets/Scenes/3 evaluation des aptitudes/search3.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using TMPro;
+
+public class search3 : MonoBehaviour
+{
+
+    public GameObject ContentHolder;
+
+    public GameObject[] Element;
+
+    public GameObject SearchBar;
+
+    public int totalElements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalElements = ContentHolder.transform.childCount;
+
+        Element = new GameObject[totalElements];
+
+        for (int i = 0; i < totalElements; i++)
+        {
+            Element[i] = ContentHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    public void Search()
+    {
+        string SearchText = SearchBar.GetComponent<TMP_InputField>().text;
+        int searchtxtlength = SearchText.Length;
+
+        int searchElements = 0;
+
+        foreach (GameObject ele in Element)
+        {
+            searchElements += 1;
+            if (ele.transform.GetComponent<TextMeshProUGUI>().text.Length >= searchtxtlength)
+            {
+                if (SearchText.ToLower() == ele.transform.GetComponent<TextMeshProUGUI>().text.Substring(0, searchtxtlength).ToLower())
+                {
+                    ele.SetActive(true);
+                }
+                else { ele.SetActive(false); }
+            }
+        }
+    }
+}
