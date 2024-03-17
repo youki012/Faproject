c8802b06effb77420110d6f02fdfe65795083bd5
youki012 <141281362+youki012@users.noreply.github.com>
2024-03-17 16:51:10 +0100

    savein sys

--- app/Assets/Scenes/TEST FOLFER/newsavesys.cs
+++ app/Assets/Scenes/TEST FOLFER/newsavesys.cs
@@ -4,32 +4,53 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
 using TMPro;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 public class newsavesys : MonoBehaviour
 {
     public string[] all;
     public TMP_InputField text;
     public string savedshit;
-    // Start is called before the first frame update
+    public string filename;
     public int x;
     public void Save()
     {
-        x = 0;
+        
         savedshit = text.text;
         File.WriteAllText(Application.dataPath + "/save.text", savedshit);
         Debug.Log("saved!");
-        while(all.Length > x)
+
+
+
+        Directory.CreateDirectory(Application.dataPath + filename);
+        x = 0;
+        File.WriteAllText(Application.dataPath + filename +"/listlength.text", all.Length.ToString());
+        File.WriteAllText(Application.dataPath + "/filename.text", filename);
+        while (all.Length > x)
         {
-            File.WriteAllText(Application.dataPath + "/save.text" + x.ToString(), all[x]);
+            File.WriteAllText(Application.dataPath + filename + "/"+ x.ToString() + ".text", all[x]);
             x++;
         }
         
     }
 
-    // Update is called once per frame
+    
     public void Load()
     {
+        
         text.text = File.ReadAllText(Application.dataPath + "/save.text");
+
+
+        x = 0;
+        filename = File.ReadAllText(Application.dataPath + "/filename.text");
+        Array.Resize(ref all, int.Parse(File.ReadAllText(Application.dataPath + filename + "/listlength.text")));
         Debug.Log("loaded!");
+        while (all.Length > x)
+        {
+            all[x] = File.ReadAllText(Application.dataPath + filename + "/" + x.ToString() + ".text");
+            x++;
+        }
     }
 }
