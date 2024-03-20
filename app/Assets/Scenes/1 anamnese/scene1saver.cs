ebd37573db7814618bac4bf034aac472775c6915
youki012 <141281362+youki012@users.noreply.github.com>
2024-03-20 23:48:57 +0100

    sex save

--- app/Assets/Scenes/1 anamnese/scene1saver.cs
+++ app/Assets/Scenes/1 anamnese/scene1saver.cs
@@ -13,23 +13,26 @@ public class scene1saver : MonoBehaviour
 {
     
     public TMP_InputField nom;
-    
+    public gender gender;
     
     
     public void Save()
     {
-
+        
         Directory.CreateDirectory(Application.dataPath + "/infopage");
-        File.WriteAllText(Application.dataPath + "/infopage" + "name.text", nom.text);       
-                          
+        File.WriteAllText(Application.dataPath + "name.text", nom.text);
+        File.WriteAllText(Application.dataPath  + "gender.text", gender.f.ToString());
+
         Debug.Log("saved!");
     }
 
 
     public void Load()
     {
-        nom.text = File.ReadAllText(Application.dataPath + "/infopage" + "name.text");        
-        
+        nom.text = File.ReadAllText(Application.dataPath + "name.text");
+        gender.females.isOn = bool.Parse(File.ReadAllText(Application.dataPath + "gender.text"));
+        gender.males.isOn = !bool.Parse(File.ReadAllText(Application.dataPath + "gender.text"));
+
         Debug.Log("loaded!");
     }
 
--- app/Assets/scripts/gender.cs
+++ app/Assets/scripts/gender.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using UnityEngine.UI;
-
+using TMPro;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
 public class gender : MonoBehaviour
 {
     // Start is called before the first frame update
     public Toggle females;
     public Toggle males;
-    bool f = false;
-    bool m = true;
-    void Start()
-    {
-        
-    }
-
+    public bool f = false;
+    public bool m = true;
+   
     // Update is called once per frame
     void Update()
     { 
-        if (females.isOn == true && males.isOn == true && m) { females.isOn = true; males.isOn = false; f = true; m = false; }
-        if (females.isOn == true && males.isOn == true && f) { females.isOn = false; males.isOn = true; m = true; f = false; }
+        if (females.isOn == true && males.isOn == true && m) {
+            females.isOn = true; males.isOn = false; f = true; m = false;
+            
+        }
 
+        if (females.isOn == true && males.isOn == true && f) {
+            females.isOn = false; males.isOn = true; m = true; f = false; }
+       
     }
 }
