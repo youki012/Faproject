98999fcef663cb8104d856d17f33f5eb8ea3a57d
youki012 <141281362+youki012@users.noreply.github.com>
2024-03-26 15:08:11 +0100

    PATIENT FILES

--- app/Assets/Scenes/TEST FOLFER/patientfiles.cs
+++ app/Assets/Scenes/TEST FOLFER/patientfiles.cs
@@ -1,19 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+using System.Linq;
+
 
 
 public class patientfiles : MonoBehaviour
 {
-    public string filename;
+    public GameObject patientlist;
+    public GameObject basepatient;
+    public TMP_InputField filename;
+    public int x = 0;
+    public int y = 0;
+    public GameObject[] patient;
     // Start is called before the first frame update
-    public void save()
+    public void newpatient()
     { 
-        if (filename != "")
+        if (filename.text != "")
         {
-            Directory.CreateDirectory(Application.streamingAssetsPath + ("/"+filename));
             
+            Directory.CreateDirectory(Application.streamingAssetsPath + ("/"+filename.text));
+            x++;
+            PlayerPrefs.SetInt("patientnumber", x);
+            (Instantiate(basepatient) as GameObject).transform.SetParent(patientlist.transform);
+            Array.Resize(ref patient, x);
+            patient[x - 1] = patientlist.transform.GetChild(x - 1).gameObject;
+            patient[x-1].transform.localScale = new Vector3(1, 1, 1);
+            patient[x-1].name = "brad" + (x-1).ToString();
+            
+
+
         }
         else
         {
@@ -22,8 +43,30 @@ public class patientfiles : MonoBehaviour
     }
     
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-       
+        x = PlayerPrefs.GetInt("patientnumber");
+        if (patientlist.transform.childCount!=0)
+        {
+            while (x > y)
+            {
+
+                Destroy(patient[y]);
+                y++;
+            }
+        }
+        y = 0;
+        Array.Resize(ref patient, x);
+     
+        while (y<x)
+        {   
+            (Instantiate(basepatient) as GameObject).transform.SetParent(patientlist.transform);
+            
+            patient[y] = patientlist.transform.GetChild(y).gameObject;
+            patient[y].transform.localScale = new Vector3(1, 1, 1);
+            patient[y].name = "brad" + y.ToString();
+            y++;
+        }
+        y = 0;
     }
 }
